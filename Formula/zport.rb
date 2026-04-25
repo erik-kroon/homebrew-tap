c336b441af9d61e389d3a4a16b74d101874a3607
erik <erik@drastic.dev>
2026-04-25 20:28:25 +0200

    Add zport formula

---
+++ Formula/zport.rb
@@ -0,0 +1,31 @@
+class Zport < Formula
+  desc "Find and kill processes using local ports"
+  homepage "https://github.com/erik-kroon/zport"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/erik-kroon/zport/releases/download/v0.1.0/zport-v0.1.0-macos-arm64.tar.gz"
+      sha256 "ae9ddfbeb44410429e8112e812333a1a37aaaafc8ef92fa9c6b97e9d8e4dda7b"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/erik-kroon/zport/releases/download/v0.1.0/zport-v0.1.0-linux-aarch64.tar.gz"
+      sha256 "58872442ef71f892e7ba72a3651ca68662477ad50714baf8169369f1a7bba52f"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/erik-kroon/zport/releases/download/v0.1.0/zport-v0.1.0-linux-x86_64.tar.gz"
+      sha256 "14dd782c99d63f18d8d59a19a4252c3804589a5995463a53d4aa61528cf7441c"
+    end
+  end
+
+  def install
+    bin.install "bin/zport"
+  end
+
+  test do
+    assert_match "zport #{version}", shell_output("#{bin}/zport --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,9 @@
+# homebrew-tap
+
+Homebrew tap for Erik Kroon projects.
+
+## zport
+
+```bash
+brew install erik-kroon/tap/zport
+```
